397dbd345a433b52bb45b69011b9125f9d5a3b87
lucifer-b16 <bharathshan@outlook.com>
2025-05-12 19:46:34 +0530

    Commit

--- q1/index.js
+++ q1/index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const app = express();
-const stockRoutes = require("./routes/stocks");
+const stockRoutes = require("./routes/stocks"); // Import the router
 
+// Middleware
 app.use(express.json());
 
-app.use("/", stockRoutes);
+// Register routes
+app.use("/", stockRoutes); // Mount the router
 
+// Start the server
 const PORT = 8000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
--- q1/routes/stocks.js
+++ q1/routes/stocks.js
@@ -1,4 +1,3 @@
-// stocks.js
 const express = require("express");
 const router = express.Router();
 const { fetchPriceHistory } = require("../utils/fetch");
